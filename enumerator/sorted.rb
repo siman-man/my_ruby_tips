1d18852a7ad19967a19cc5f67f533de803307820
siman-man <k128585@ie.u-ryukyu.ac.jp>
2017-03-08 22:47:00 +0900

    add sorted? - sorted_by?

---
+++ enumerator/sorted.rb
@@ -0,0 +1,10 @@
+module Enumerable
+  def sorted?
+    each_cons(2).all? {|a, b| (a <=> b) <= 0 }
+  end
+
+  def sorted_by?
+    each_cons(2).all? {|a, b| (yield(a) <=> yield(b)) <= 0 }
+  end
+end
+
