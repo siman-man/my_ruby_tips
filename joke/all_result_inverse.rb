d6f67dd2110a5cb74344aa4b8fcf365565831bb9
siman-man <k128585@ie.u-ryukyu.ac.jp>
2017-04-02 01:33:24 +0900

    update april joke

--- joke/all_result_inverse.rb
+++ joke/all_result_inverse.rb
@@ -1,30 +1,28 @@
-# RUBY_VERSION >= 2.3.0
-klasses = ObjectSpace.each_object(Class).reject {|k| k.singleton_class? }.sort_by(&:to_s)
+klasses = ObjectSpace.each_object(Module).reject {|k| k.singleton_class? }.sort_by(&:to_s)
 klasses -= [ARGF.class, BasicObject]
 
 methods = {}
 
 klasses.reject! do |k|
-  methods[k] = k.instance_methods(false).select {|m| m.to_s.end_with?('?') }
+  methods[k] = k.instance_methods(false).select {|m| m.to_s.end_with?('?') } - [:respond_to?]
   methods[k].empty?
 end
 
 klasses.each do |k|
-  mname = "#{k}AprilFool"
-  eval("module #{mname}; end")
+  k.class_eval(
+    methods[k].map do |name|
+      <<-EOF
+      alias_method "_#{name}", :#{name}
 
-  m = Object.const_get(mname)
-
-  m.module_eval do
-    methods[k].each do |name|
-      define_method(name) {|*args, &block| !super(*args, &block) }
-    end
-  end
-
-  k.class_eval { prepend m }
+      def #{name}(*args, &block)
+        !_#{name}(*args, &block)
+      end
+      EOF
+    end.join("\n")
+  )
 end
 
-p 1.positive?             #=> false
-p 1.negative?             #=> true
 p (1..10).include?(3)     #=> false
 p (1..10).include?(100)   #=> true
+p [1, 2, 3].all?(&:odd?)  #=> true
+p [1, 2, 3].any?(&:odd?)  #=> false
